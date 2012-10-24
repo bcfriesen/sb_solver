20b6837d8748b6e01dc1e19afdcd1db0549d6d65
Brian Friesen <friesen@ou.edu>
2012-10-24 09:45:24 -0500

    Switched from kind() to selected_real_kind(). Don't know if there's any function al difference but the latter is newer I think.

--- src/machine.f90
+++ src/machine.f90
@@ -1,6 +1,6 @@
 ! Machine-dependent constants.
 MODULE machine
   IMPLICIT NONE
-  INTEGER, PARAMETER :: sp = kind(1.0)
-  INTEGER, PARAMETER :: dp = kind(1.0D0)
+  INTEGER, PARAMETER :: sp = selected_real_kind(precision(1.0))
+  INTEGER, PARAMETER :: dp = selected_real_kind(precision(1.0d0))
 END MODULE machine
